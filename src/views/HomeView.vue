fc72a98a44672e5ca39f3afc72b26a8b46c4c0f4
aeyll-11 <taguine.idriss@gmail.com>
2024-05-10 12:51:30 +0200

    feat: add PageTree to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PageTree from '@/components/PageTree.vue';
+</script>
 
 <template>
   <main>
     <p>Please build the structure you see in <code>PageTree.png</code> in the file explorer.</p>
     <p>The data for this structure should be loaded from <code>fetchData.js</code>.</p>
     <p>Useful icon characters are ▼ ▶ •</p>
+    <PageTree />
   </main>
 </template>
