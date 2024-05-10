caa7b5d35957a8d6e9628ea7f3fbf663f17cde66
aeyll-11 <taguine.idriss@gmail.com>
2024-05-10 12:40:51 +0200

    feat: test instructions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main></main>
+  <main>
+    <p>Please build the structure you see in <code>PageTree.png</code> in the file explorer.</p>
+    <p>The data for this structure should be loaded from <code>fetchData.js</code>.</p>
+    <p>Useful icon characters are ▼ ▶ •</p>
+  </main>
 </template>
