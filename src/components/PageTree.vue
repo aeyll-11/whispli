d596925b7134c4994588baa31f6b5854a57c403a
aeyll-11 <taguine.idriss@gmail.com>
2024-05-10 12:47:09 +0200

    feat: add PageTree and PageTreeItem component

---
+++ src/components/PageTree.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { fetchData } from '@/composables/fetchData';
+import type { Data } from '@/interfaces/data.interface';
+import { onMounted, ref } from 'vue';
+import PageTreeItem from './PageTreeItem.vue';
+const items = ref<Data[]>([]);
+
+onMounted(async () => {
+  items.value = await fetchData();
+});
+</script>
+
+<template>
+  <div>
+    <PageTreeItem v-for="item in items" :data="item" :key="item.id" />
+  </div>
+</template>
---
+++ src/components/PageTreeItem.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import type { Data } from '@/interfaces/data.interface';
+import { computed, ref } from 'vue';
+
+const props = defineProps<{ data: Data }>();
+
+const hasChildren = computed<boolean>(() => Array.isArray(props.data.children));
+
+/**
+ * Handle icon display logic
+ */
+const icon = computed(() => {
+  if (hasChildren.value) {
+    return '▶';
+  }
+  return '•';
+});
+
+const isOpen = ref<boolean>(false);
+
+const toggle = (): void => {
+  if (hasChildren.value) {
+    isOpen.value = !isOpen.value;
+  }
+};
+</script>
+
+<template>
+  <ul></ul>
+</template>
+
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
